ed1dedf30038075f3b65e349ecf39082c70c3927
Michal Torma <michal.torma@nhm.uio.no>
2024-05-22 11:39:43 +0200

    expand hotfix

--- endpoint-hotfix.sh
+++ endpoint-hotfix.sh
@@ -6,6 +6,14 @@ BASE_PATH="/7642a087_hass-n8n"
 # Directory where n8n static files are located
 N8N_DIR="/usr/local/lib/node_modules/n8n/dist"
 
-# Find and replace the base path in the HTML and JavaScript files
+# Update HTML files
 find $N8N_DIR -type f -name "*.html" -exec sed -i "s|/assets/|$BASE_PATH/assets/|g" {} +
+
+# Update JavaScript files
 find $N8N_DIR -type f -name "*.js" -exec sed -i "s|/assets/|$BASE_PATH/assets/|g" {} +
+
+# Update CSS files
+find $N8N_DIR -type f -name "*.css" -exec sed -i "s|/assets/|$BASE_PATH/assets/|g" {} +
+
+# Additionally, you might need to update other types of files, for instance, JSON or other configuration files
+find $N8N_DIR -type f -name "*.json" -exec sed -i "s|/assets/|$BASE_PATH/assets/|g" {} +
